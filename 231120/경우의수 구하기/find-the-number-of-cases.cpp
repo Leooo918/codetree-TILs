7ce23e7f20405a2d0cd82762f18b0634e26b3472
Leooo <123617759+Leooo918@users.noreply.github.com>
2023-11-20 19:12:59 +0900

    Title: 경우의수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231120/경우의수 구하기/find-the-number-of-cases.cpp
+++ 231120/경우의수 구하기/find-the-number-of-cases.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
-    int num = 1;
+    int num = 0;
 
     for(int i = b - a; i > 0; i--){
         num += b - i;
     }
+
+    if(num == 0) num ++;
 
     cout << num;
 
