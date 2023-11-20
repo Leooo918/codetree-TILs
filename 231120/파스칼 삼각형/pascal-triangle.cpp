d1ac996010af58e1bda39c7d2d6a3096c9803aa9
Leooo <123617759+Leooo918@users.noreply.github.com>
2023-11-20 19:43:41 +0900

    Title: 파스칼 삼각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231120/파스칼 삼각형/pascal-triangle.cpp
+++ 231120/파스칼 삼각형/pascal-triangle.cpp
@@ -3,40 +3,36 @@
 
 using namespace std;
 int main() {
-    int r,c,w;
+    int r, c, w;
     vector<vector<int>> pascalTriangle;
 
     int total = 0;
 
     cin >> r >> c >> w;
 
-    for(int i = 0; i < r + w; i++){
-            vector<int> a;
-        for(int j = 0; j < i; j++){
-            if(i == 0){
+    for (int i = 1; i <= r + w; i++) {
+        vector<int> a;
+        for (int j = 0; j < i; j++) {
+            if (i == 0) {
                 a.push_back(1);
             }
-            else{
-                if(j == 0 || j == i){
+            else {
+                if (j == 0 || j == i - 1) {
                     a.push_back(1);
                 }
-                else{
-                    a.push_back(pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j]);
+                else {
+                    a.push_back(pascalTriangle[i - 2][j - 1] + pascalTriangle[i - 2][j]);
                 }
             }
-            cout << a[j] << " ";
         }
         pascalTriangle.push_back(a);
-        cout << "\n";
     }
 
-    for(int i = 0; i <= w; i++){
-        for(int j = 0; j <= i; j++){
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j <= i; j++) {
             total += pascalTriangle[r + i - 1][c + j - 1];
-            cout <<r + i - 1<< " " <<c + j - 1<< " " << pascalTriangle[r + i][c + j] << "\n";
         }
     }
-    cout << "\n";
 
     cout << total;
 
