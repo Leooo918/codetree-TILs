2ea29fa438a761764d9d1b6f748d16a2d6284371
Leooo <123617759+Leooo918@users.noreply.github.com>
2023-11-21 19:04:03 +0900

    Title: 연쇄로 터지는 폭탄, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231121/연쇄로 터지는 폭탄/series-of-bombs-detonating.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    
+     int n;
+    int answer = 0;
+    vector<int> input;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        input.push_back(a);
+    }
+    int maxNum = 0;
+
+    for (int i = 0; i < n; i++) {
+        int curRange = 1;
+        int curNum = i;
+        int curSelected = 1;
+        for (int j = 0; j < n; j++) {
+            bool breakCheck = true;
+            for (int k = 1; k <= curRange; k++) {
+                if (find(input.begin(), input.end(), input[curNum] + k) != input.end()) {
+                    curNum = find(input.begin(), input.end(), input[curNum] + k) - input.begin();
+                    ++curSelected;
+                    curRange++;
+                    breakCheck = false;
+                }
+            }
+            if (breakCheck) break;
+        }
+
+        curNum = i;
+        curRange = 1;
+        for (int j = 0; j < n; j++) {
+            bool breakCheck = true;
+            for (int k = 1; k <= curRange; k++) {
+                if (find(input.begin(), input.end(), input[curNum] - k) != input.end()) {
+                    curNum = find(input.begin(), input.end(), input[curNum] - k) - input.begin();
+                    ++curSelected;
+                    ++curRange;
+                    breakCheck = false;
+                }
+            }
+            if (breakCheck) break;
+        }
+
+
+
+        if (maxNum <= curSelected) {
+            maxNum = curSelected;
+        }
+    }
+
+    cout << maxNum;
+
+
+    return 0;
+}
