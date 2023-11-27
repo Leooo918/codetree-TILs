5646cd32b389bdd4d1674aeec8778b229d3df97e
Leooo <123617759+Leooo918@users.noreply.github.com>
2023-11-27 19:13:43 +0900

    Title: 함수 실행 횟수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231127/함수 실행 횟수/number-of-function-executions.cpp
+++ 231127/함수 실행 횟수/number-of-function-executions.cpp
@@ -12,12 +12,10 @@ int main()
 	cin >> n;
 
 	dp.push_back(1);
-	dp.push_back(1);
-	dp.push_back(3);
+	dp.push_back(1);	
 
-
-	for (int i = 3; i <= n; i++) {
-		dp.push_back(dp[i - 3] + dp[i - 2] + dp[i - 1]);
+	for (int i = 2; i <= n; i++) {
+		dp.push_back(dp[i - 1] +dp[i - 2] + 1);
 	}
 
 	cout << dp[n];
