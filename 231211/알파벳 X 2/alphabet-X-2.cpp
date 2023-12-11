2e71d1f315edb12a48de2a5262063876cee87ee9
Leooo <123617759+Leooo918@users.noreply.github.com>
2023-12-11 18:24:50 +0900

    Title: 알파벳 X 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231211/알파벳 X 2/alphabet-X-2.cpp
+++ 231211/알파벳 X 2/alphabet-X-2.cpp
@@ -9,14 +9,30 @@ int main() {
     cin >> input;
 
     int total = 0;
+    vector<int> v(52);
 
-    for(int i = 0; i < input.size(); i++){
-        if(input[i] != input[++i]){
-            total++;
+    for(int i = 0; i < 26; i++){
+        int position = -1;
+        for(int j = 0; j < input.size(); j++){
+            if(position >= 0) position++;
+            if(input[j] == 'A' + i){
+                if(position < 0)position = 0;
+                else break;
+            }
+        }
+        v[position]++;
+    }
+
+    int n = max_element(v.begin(), v.end()) - v.begin();
+    int m = 0;
+
+    for(int i = 0; i < v.size();i++){
+        if(i != n){
+            m += v[i];
         }
     }
 
-    cout << total / 2;
+    cout << m / 2;
 
     return 0;
 }
